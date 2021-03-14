e74b59f0613f41d17d4f9b834976677bae6ec039
ruofei <rf.wangchn@foxmail.com>
2021-03-15 00:14:47 +0800

    feat: 移除 sass 改用 less

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import './index.scss';
+import './index.less';
 
 ReactDOM.render(
   <React.StrictMode>
