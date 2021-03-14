39d13a804e2338c1238c085f8e81ed3995e9c303
ruofei <rf.wangchn@foxmail.com>
2021-03-15 00:21:11 +0800

    feat: 安装 craco，定制 antd 主题

---
+++ craco.config.js
@@ -0,0 +1,19 @@
+/* craco.config.js */
+
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@font-size-base': '16px' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
