90d5d5a62924e5b6765d13dc40269519b684b5d2
ruofei <rf.wangchn@foxmail.com>
2021-03-14 10:32:48 +0800

    feat: 安装配置 electron

---
+++ electron/index.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require("electron");
+
+const { NODE_ENV } = process.env;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  if (NODE_ENV === "development") win.loadURL("http://localhost:3000");
+  else win.loadFile("build/index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
